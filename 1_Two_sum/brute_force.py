2ffe327579c66c0cf3beaf54771db37a7d8c78ae
Saurabh Kumar Giri <salesforce.giri@extaccount.com>
2025-08-01 23:30:40 +0530

    1 Two Sum

---
+++ 1_Two_sum/brute_force.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums, target: int):
+        l = len(nums)
+        
+        for i in range(l):
+            for j in range(i+1, l):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+        
